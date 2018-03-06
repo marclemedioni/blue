1bd6035cbe64eadbec20738b1627a1e7c4b2d863
Marc <marc@docorga.com>
2018-03-06 16:02:16 +0100

    Remove vote system to stop musicplayer

--- src/commands/music/Stop.ts
+++ src/commands/music/Stop.ts
@@ -24,24 +24,13 @@ export class StopCommand extends Command<Bot> {
       return;
     }
 
-		const amount: number = queue.voice.members.size;
-		const voter: GuildMember = message.member;
-
-		if (!queue.voters.includes(voter.id)) {
-			queue.voters.push(voter.id);
-			if (queue.voters.length >= Math.ceil(amount / 2)) {
-				try {
-					queue.videos = [];
-					queue.dispatcher.end();
-					queue.voters = [];
-				} catch (err) {
-					return message.channel.send("Unable to stop the queue.");
-				}
-				return message.channel.send(`Stopped queue, **${queue.videos.length}** videos have been skipped`);
-			}
-			return message.channel.send("Vote added.");
-		} else {
-			return message.channel.send("You have already voted to stop");
-		}
+		try {
+      queue.videos = [];
+      queue.dispatcher.end();
+      queue.voters = [];
+    } catch (err) {
+      return message.channel.send("Unable to stop the queue.");
+    }
+    return message.channel.send(`Stopped queue, **${queue.videos.length}** videos have been skipped`);
 	}
 }
